f5f8713aca99fec117d9ec6fd6891778ee38de2f
Patricia Kronowetter <pkronowetter@student.tgm.ac.at>
2014-03-07 09:15:11 +0100

    Gruss Methode

---
+++ src/pkronowetter/Gruß.java
@@ -0,0 +1,17 @@
+package pkronowetter;
+
+
+public class Gruﬂ{
+	
+	public static void begruesung (){
+		System.out.println("Hallo lieber User");
+	}
+	
+	public static void begruesung2(){
+		System.out.println("Wie geht es ihnen heute");
+	}
+	
+	public static void abschied(){
+		System.out.println("Auf Wiedersehen");
+	}
+}
---
+++ src/pkronowetter/main.java
@@ -0,0 +1,14 @@
+package pkronowetter;
+
+public class main {
+
+	public static void main(String[] args) {
+		Gruﬂ g = new Gruﬂ();
+		
+		g.begruesung();
+		g.begruesung2();
+		g.abschied();
+
+	}
+
+}
